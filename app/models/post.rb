842e50bce59bb690547b5f38adad9d1e44da4fd9
Michael Bryant <mikeakakbe@gmail.com>
2018-04-11 13:08:31 -0400

    event changes image size changes

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,7 @@ class Post < ApplicationRecord
     :thumb => "100x100#",
     :small  => "150x150>",
     :medium => "200x200",
-    :square => "300x300#"}
+    :square => "300x300#",
+    :large => "1920x960"}
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end

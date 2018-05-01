a52094b92382bfccfdba86072958abb70b593cd1
Michael Bryant <mikeakakbe@gmail.com>
2018-05-01 15:21:03 -0400

    http protocol

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,8 @@ Rails.application.configure do
     secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
     s3_region: ENV.fetch('AWS_REGION'),
   },
-  url: "s3_domain_url"
+  url: "s3_domain_url",
+  :s3_protocol => :https
 }
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?

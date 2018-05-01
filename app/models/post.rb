d83f7b24563ecec3ec92483d5f8bee0dfc007b4d
Michael Bryant <mikeakakbe@gmail.com>
2018-05-01 12:38:22 -0400

    post model s3 config:

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,11 +2,13 @@ class Post < ApplicationRecord
   validates :title, presence: true, length: { minimum: 5 }
   validates :body, presence: true
   has_attached_file :image,
-  :styles => {
-    :thumb => "100x100#",
-    :small  => "150x150>",
-    :medium => "200x200",
-    :square => "300x300#",
-    :large => "1920x960"}
+    :download,
+    :storage => :s3,
+    :styles => {
+      :thumb => "100x100#",
+      :small  => "150x150>",
+      :medium => "200x200",
+      :square => "300x300#",
+      :large => "1920x960"}
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end

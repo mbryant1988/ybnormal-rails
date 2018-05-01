b70b0e95f75920fa8df5350dd2735b2610d6367e
Michael Bryant <mikeakakbe@gmail.com>
2018-05-01 15:37:01 -0400

    no url config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,6 @@ Rails.application.configure do
     secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
     s3_region: ENV.fetch('AWS_REGION'),
   },
-  url: "s3_domain_url",
   :s3_protocol => :https,
   :s3_permissions => "private"
 
